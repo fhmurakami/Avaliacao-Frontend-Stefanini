57d70686e7638302b061bd8785e727988abdd325
Felipe Murakami <fhmurakami@gmail.com>
2018-10-05 15:10:07 -0300

    JavaScript - Desafio

--- javascript/index.js
+++ javascript/index.js
@@ -41,7 +41,7 @@ function windowToggle(marker, infoWindow) {
     google.maps.event.addListener(infoWindow, 'closeclick', function () {
         marker.setIcon(customIcon)
     })
-    infoWindow.open(map, marker);
+    infoWindow.open(map, marker)
 }
 
 function addMarker(marker) {
@@ -57,21 +57,80 @@ function addMarker(marker) {
 }
 
 function markerClick(marker, infoWindow) {
-        // Eventos ao clicar
-        google.maps.event.addListener(marker, 'click', (function (marker) {
-            return function () {
-                infoWindow.setContent('<strong>' + marker.title + '</strong>')
-                windowToggle(marker, infoWindow)
-    
-                for (var j = 0; j < markersArray.length; j++) {
-                    markersArray[j].setIcon(customIcon)
-                }
-    
-                marker.setIcon(customIcon2)
-    
+    // Eventos ao clicar
+    google.maps.event.addListener(marker, 'click', (function (marker) {
+        return function () {
+            infoWindow.setContent('<strong>' + marker.title + '</strong>')
+            windowToggle(marker, infoWindow)
+
+            for (var j = 0; j < markersArray.length; j++) {
+                markersArray[j].setIcon(customIcon)
             }
-        })(marker))
-        markersArray.push(marker)
+
+            marker.setIcon(customIcon2)
+
+        }
+    })(marker))
+    markersArray.push(marker)
+}
+
+function CenterControl(controlDiv, map, infoWindow) {
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div')
+    controlUI.style.backgroundColor = '#fff'
+    controlUI.style.border = '2px solid #fff'
+    controlUI.style.borderRadius = '3px'
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)'
+    controlUI.style.cursor = 'pointer'
+    controlUI.style.marginBottom = '22px'
+    controlUI.style.textAlign = 'center'
+    controlUI.title = 'Click to show your position'
+    controlDiv.appendChild(controlUI)
+
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div')
+    controlText.style.color = 'rgb(25,25,25)'
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif'
+    controlText.style.fontSize = '16px'
+    controlText.style.lineHeight = '38px'
+    controlText.style.paddingLeft = '5px'
+    controlText.style.paddingRight = '5px'
+    controlText.innerHTML = 'Find Me'
+    controlUI.appendChild(controlText)
+
+    controlClick(controlUI, map, infoWindow)
+}
+
+function controlClick(controlUI, map, infoWindow) {
+     // Setup the click event listeners: set the map to user location.
+     controlUI.addEventListener('click', function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var userLocation = {
+                    name: 'You\'re Here',
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                }
+
+                var marker = addMarker(userLocation)
+                markerClick(marker, infoWindow)
+                map.setCenter(userLocation)
+            }, function () {
+                handleLocationError(true, infoWindow, map.getCenter())
+            })
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter())
+        }
+})
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos)
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.')
+    infoWindow.open(map)
 }
 
 function initMap() {
@@ -94,11 +153,19 @@ function initMap() {
 
     var infoWindow = new google.maps.InfoWindow()
 
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map'), mapOptions)
 
     //Adicionando todos os marcadores
     placesOfInterest.forEach(function (place) {
         var marker = addMarker(place)
         markerClick(marker, infoWindow)
     })
+
+    // Create the DIV to hold the control and call the CenterControl()
+    // constructor passing in this DIV.
+    var centerControlDiv = document.createElement('div')
+    var centerControl = new CenterControl(centerControlDiv, map, infoWindow)
+
+    centerControlDiv.index = 1
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv)
 }
