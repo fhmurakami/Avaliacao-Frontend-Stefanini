8946fe6f04728b8459fe3e8a25188fc37b966c09
Felipe Murakami <fhmurakami@gmail.com>
2018-10-06 20:08:44 -0300

    Angular - Questão 3a

--- angular/src/app/list/list.page.ts
+++ angular/src/app/list/list.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { legends } from '../names/nameslist'
+
 @Component({
   selector: 'app-list',
   templateUrl: 'list.page.html',
   styleUrls: ['list.page.scss']
 })
 export class ListPage {
-  legends: Array<string> = [];
+  legends: Array<string> = legends;
 
   ngOnInit() {
 
