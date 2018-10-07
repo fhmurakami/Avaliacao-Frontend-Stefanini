06d638a1f35a4bd437bc2766e62a5fb6f4020e06
Felipe Murakami <fhmurakami@gmail.com>
2018-10-06 23:00:07 -0300

    Angular - Questão 3b

--- angular/src/app/search/search.page.ts
+++ angular/src/app/search/search.page.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 
+import { legends } from '../names/nameslist'
+
 @Component({
   selector: 'app-search',
   templateUrl: 'search.page.html',
   styleUrls: ['search.page.scss']
 })
 export class SearchPage {
-  searchInput : string;
+  searchInput : string = '';
   results = [];
 
   ngOnInit() {
   }
 
-  onSearch() {
-    console.log(`Search: ${this.searchInput}`)
+  onSearch(event) {
     
+    let val = event.target.value
+
+    if (val && val.trim() !== ''){
+      this.results = legends.filter(function(name) {
+        return name.toLowerCase().includes(val.toLowerCase())
+      })
+    } 
+    else {
+      this.results = []
+    }
   }
 }
