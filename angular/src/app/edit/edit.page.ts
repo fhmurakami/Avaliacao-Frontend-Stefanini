966ceb9af3346a5e39d214f9de8f71bfa9bf9063
Felipe Murakami <fhmurakami@gmail.com>
2018-10-08 23:34:59 -0300

    Angular - Questão 3c e 3d

--- angular/src/app/edit/edit.page.ts
+++ angular/src/app/edit/edit.page.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
 
+import { legends } from '../names/nameslist'
+
 @Component({
   selector: 'app-edit',
   templateUrl: 'edit.page.html',
   styleUrls: ['edit.page.scss']
 })
 export class EditPage {
+  legends: Array<string> = legends;
 
-  constructor() {
-    
+  edit(event, id) {
+    const input = event.target.previousElementSibling
+
+    if (event.target.innerHTML === 'EDIT') {
+      event.target.innerHTML = 'SAVE'
+      input.removeAttribute("readonly")
+      input.focus()
+    } else {
+      this.save(input, id)
+      input.setAttribute("readonly", "")
+      event.target.innerHTML = 'EDIT'
+    }
   }
 
+  save(element, id) {
+    legends[id] = element.value
+  }
+
+  constructor() {
+
+  }
 }
 
 
